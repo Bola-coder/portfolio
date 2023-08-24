301408c9bf099d86b636b6e1629cd01e7ead7211
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2023-08-24 18:50:59 +0100

    Added anonify to the project list and removed clones

--- src/components/projectArray.js
+++ src/components/projectArray.js
@@ -1,13 +1,24 @@
 import texbuy from "./../images/project_images/texbuy.png";
-import netflixClone from "./../images/project_images/netflix-clone.png";
+// import netflixClone from "./../images/project_images/netflix-clone.png";
 // import planktops from "./../images/project_images/planktops.png";
 import recipea from "./../images/project_images/recipea.png";
 import texchange from "./../images/project_images/texchange.png";
-import twitterClone from "./../images/project_images/twitter-clone.png";
-import netflixLanding from "./../images/project_images/netflix-landing.png";
+// import twitterClone from "./../images/project_images/twitter-clone.png";
+// import netflixLanding from "./../images/project_images/netflix-landing.png";
+import anonify from "./../images/project_images/anonifyImage.png";
 // import randGen from "./../images/project_images/randgen.png";
 
 const projects = [
+  {
+    name: "Anonify",
+    tags: ["React", "NextJs", "Web-app"],
+    image: anonify,
+    description:
+      "An anonymous web messaging platform to send and recieve anonymous messages from friends and loved ones",
+    liveLink: "https://anonify.netlify.app",
+    githubLink: "https://github.com/Bola-coder/Anonify",
+    author: "Bolarinwa Ahmed",
+  },
   {
     name: "TexBuy",
     tags: ["React", "Web-app"],
@@ -16,18 +27,18 @@ const projects = [
       "A product E-commerce Store where products be added to cart once user is authenticated",
     liveLink: "https://texbuy.netlify.app",
     githubLink: "https://github.com/Bola-coder/Sneakers",
-    author: "Ahmed Bolarinwa",
+    author: "Bolarinwa Ahmed",
   },
-  {
-    name: "Netflix Clone",
-    tags: ["React", "Clone"],
-    image: netflixClone,
-    description:
-      "A clone of Netflix Webapp that fetches latest movies from an external API",
-    liveLink: "https://abatoms-netflix-clone.netlify.app/",
-    githubLink: "https://github.com/Bola-coder/React-Netflix-Clone",
-    author: "Ahmed Bolarinwa",
-  },
+  // {
+  //   name: "Netflix Clone",
+  //   tags: ["React", "Clone"],
+  //   image: netflixClone,
+  //   description:
+  //     "A clone of Netflix Webapp that fetches latest movies from an external API",
+  //   liveLink: "https://abatoms-netflix-clone.netlify.app/",
+  //   githubLink: "https://github.com/Bola-coder/React-Netflix-Clone",
+  //   author: "Bolarinwa Ahmed",
+  // },
 
   {
     name: "TexChange",
@@ -37,19 +48,19 @@ const projects = [
       "A Currency converter web application that uses currency API to convert currencies",
     liveLink: "https://texchange.netlify.app/",
     githubLink: "https://github.com/Bola-coder/Xchange",
-    author: "Ahmed Bolarinwa",
+    author: "Bolarinwa Ahmed",
   },
 
-  {
-    name: "Twitter Clone",
-    tags: ["React", "Clone"],
-    image: twitterClone,
-    description:
-      "A clone of Twitter Webapp with messaging capabilities using firebase firestore",
-    liveLink: "https://twitter-clone-90ea9.web.app/",
-    githubLink: "https://github.com/Bola-coder/react-twitter-clone",
-    author: "Ahmed Bolarinwa",
-  },
+  // {
+  //   name: "Twitter Clone",
+  //   tags: ["React", "Clone"],
+  //   image: twitterClone,
+  //   description:
+  //     "A clone of Twitter Webapp with messaging capabilities using firebase firestore",
+  //   liveLink: "https://twitter-clone-90ea9.web.app/",
+  //   githubLink: "https://github.com/Bola-coder/react-twitter-clone",
+  //   author: "Bolarinwa Ahmed",
+  // },
 
   {
     name: "Recipea",
@@ -59,17 +70,17 @@ const projects = [
       "Website for looking up recipes. Search function is also included",
     liveLink: "https://abatoms-recipe-app.netlify.app/",
     githubLink: "https://github.com/Bola-coder/react-recipe-app",
-    author: "Ahmed Bolarinwa",
+    author: "Bolarinwa Ahmed",
   },
-  {
-    name: "Netflix Landing Page Clone",
-    tags: ["HTML", "CSS", "JS", "CLONE"],
-    image: netflixLanding,
-    description: "A simple clone of netflix landing page",
-    liveLink: "https://bola-coder.github.io/Netflix-Landing-Page-Clone/",
-    githubLink: "  https://github.com/Bola-coder/Netflix-Landing-Page-Clone",
-    author: "Ahmed Bolarinwa",
-  },
+  // {
+  //   name: "Netflix Landing Page Clone",
+  //   tags: ["HTML", "CSS", "JS", "CLONE"],
+  //   image: netflixLanding,
+  //   description: "A simple clone of netflix landing page",
+  //   liveLink: "https://bola-coder.github.io/Netflix-Landing-Page-Clone/",
+  //   githubLink: "  https://github.com/Bola-coder/Netflix-Landing-Page-Clone",
+  //   author: "Bolarinwa Ahmed",
+  // },
 ];
 
 export default projects;
